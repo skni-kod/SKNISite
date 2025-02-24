9fa96a42ba9068ee9429952acafbbbce3af6a7a7
Sebastian Zieliński <zielis277@wp.pl>
2025-02-24 23:46:59 +0100

    equipment component - background fix
    Now component uses next/image

--- skni_site_frontend/src/app/(home)/(components)/equipment.tsx
+++ skni_site_frontend/src/app/(home)/(components)/equipment.tsx
@@ -1,15 +1,24 @@
 import * as React from "react";
 import { useTranslations } from "next-intl";
-import {Text} from "./text"
-import {Button} from "./button";
+import {Text} from "@/components/ui/text"
+import {Button} from "@/components/ui/button";
 import Link  from "next/link";
+import Image from "next/image";
+
 
 const Equipment: React.FC = () => {
     const t = useTranslations('HomePage.Equipment');
 
     return(
-    <div className="flex min-h-[600px] w-full bg-cover bg-center justify-center" style = {{backgroundImage: "url('equipment.jpg')" }}>
-        <div className="max-w-[600px] w-4/5 space-y-8 mt-16 mb-16">
+    <div className="relative w-screen flex justify-center">
+        <Image
+            src="/equipment.jpg"
+            alt="Equipment background"
+            fill
+            className="object-cover"
+            priority
+        />
+        <div className="relative max-w-[600px] w-4/5 space-y-10 mt-12 mb-16">
             <Text variant="h1" className="text-white">
                 {t("title")}
             </Text>
@@ -20,7 +29,7 @@ const Equipment: React.FC = () => {
                 {t("paragraph2")}
             </Text>
             <Link href="/about" className="block">
-                <Button asChild variant="default" size="lg" className="bg-white rounded-3xl border-[2px] text-[#55ACEE]
+                <Button asChild variant="default" size="lg" className="bg-white rounded-3xl border-2 text-[#55ACEE]
                 hover:bg-transparent hover:text-white hover:border-white duration-300">
                     <span>{t("button")}</span>
                 </Button>
